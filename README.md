9e887f328612af62c16617aa90bcbaafa734244e
Chris Carrel <ccarrel@virtualhaven.net>
2024-03-13 09:44:21 -0400

    Initial Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Composer-enabled Wordpress Upstream for WP Engine
+
+This configuration borrows logic from Pantheon's upstream process and leverages GitHub actions to maintain a site on WP Engine without storing WordPress core or hosted plugins in the git repo.
---
+++ scripts/SyncHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alquemie\composer;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class SyncHandler
+{
+
+  public static function sourceFiles() {
+    $fs = new Filesystem();
+    $root = getcwd();
+
+    if (!$fs->exists($root . '/public/wp-content')) {
+      $fs->mkdir($root . '/public/wp-content');
+    }
+
+    $fs->mirror($root . '/src/wp-content', $root . '/public/wp-content');
+
+    if (!$fs->exists($root . '/public/root')) {
+      $fs->mkdir($root . '/public/root');
+    }
+    
+    $fs->mirror($root . '/src/root', $root . '/public/root');
+
+  }
+  
+  public static function lastUpdated() {
+    $entry = date('l | jS \of F Y h:i:s A', time())."\n";
+    $root = getcwd();
+    file_put_contents($root . '/src/root/.gitkeep', $entry); 
+  }
+  
+}
